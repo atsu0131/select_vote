55f62e79cc8e8cf51f03461a4e19a2c4dc0b617d
atsu0131 <atsushi.isai131@gmail.com>
2018-11-24 14:09:41 +0900

    ログイン機能作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :voters, dependent: :destroy
   has_many :politicians, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
