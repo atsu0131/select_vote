75275d0ef5511895a6b9a0db3d3a6fdd1eeb894d
atsu0131 <atsushi.isai131@gmail.com>
2018-11-25 10:18:30 +0900

    user_controller作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,19 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
   def new
     @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
 
   def create
     @user = User.new(user_params)
@@ -13,6 +22,22 @@ class Admin::UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_user_path
   end
 
   private
