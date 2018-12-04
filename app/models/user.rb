54b563531d07e817df5726b8a0f564c133e236bb
atsu0131 <atsushi.isai131@gmail.com>
2018-12-04 11:31:42 +0900

    user_id追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :voters, dependent: :destroy
   has_many :politicians, dependent: :destroy
+  has_many :blogs, dependent: :destroy
   belongs_to :pref
   belongs_to :zone
 
